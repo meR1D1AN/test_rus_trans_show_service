2a217de8703c881c351b1eaab021ad5b5c6af0e2
Никита Шидогубов <mered1an@live.com>
2025-09-25 22:04:19 +0300

    [~] Обновление docker-compose.yaml

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ USE_L10N = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+# STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
